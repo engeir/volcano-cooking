1a79c40ff320d2d875942fca3153c70fa34e9cca
engeir <eirroleng@gmail.com>
2021-09-01 12:39:43 +0200

    Fix typo

--- src/volcano_cooking/view_force.py
+++ src/volcano_cooking/view_force.py
@@ -1,4 +1,4 @@
-"""CLI for the view_synthetic_forcing function.
+"""CLI for the view_generated_forcing function.
 
 This implements a wrapper for a function to be able to use it as a standalone program. It
 takes a filename as a mandatory input parameter.
