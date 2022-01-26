63e4c88836389bd07b0de87192217024e42f6246
engeir <eirroleng@gmail.com>
2022-01-26 13:26:00 +0100

    Add comment

--- src/volcano_cooking/modules/create/create_data.py
+++ src/volcano_cooking/modules/create/create_data.py
@@ -313,6 +313,7 @@ class Generate(ABC):
         maximum injection heights.
         """
         self.gen_dates_totalemission_vei()
+        # Shift first eruption to occur before initial year
         self.yoes -= abs(self.init_year - self.yoes[0]) + 1
         if len(self.veis) != self.size:
             self.size = len(self.veis)
