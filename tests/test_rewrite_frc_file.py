02bee28c435371563cff5c5afd7a14b4986f9a46
engeir <eirroleng@gmail.com>
2022-02-15 19:03:59 +0100

    Split up tests into separate funcs
    * Testing for different things are split up from one function for all,
      to one function per case.
    * Adds TODO for possibly writing up a test that checks the procedure of
      creating a new file by re-writing the original forcing file.

--- tests/test_rewrite_frc_file.py
+++ tests/test_rewrite_frc_file.py
@@ -22,8 +22,8 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_create_volcaoes(runner: CliRunner) -> None:
-    """Test to see if files are created as expected.
+def test_file_exists(runner: CliRunner) -> None:
+    """Test to see if expected error is raised when file does not exist.
 
     Parameters
     ----------
@@ -34,8 +34,17 @@ def test_create_volcaoes(runner: CliRunner) -> None:
         with pytest.raises(FileNotFoundError):
             synthetic_volcanoes.create_volcanoes(version=2, option=1)
 
+
+def test_forcing_file_indexes(runner: CliRunner) -> None:
+    """Test to see if expected error is raised when forcing file indexes are wrong.
+
+    Parameters
+    ----------
+    runner: CliRunner
+        Runner for creating isolated file  system.
+    """
     with runner.isolated_filesystem():
-        # Create fake source data set
+        # Create bad source data set
         os.mkdir("data")
         os.mkdir(os.path.join("data", "originals"))
         file = os.path.join(
@@ -47,3 +56,34 @@ def test_create_volcaoes(runner: CliRunner) -> None:
         ds1.to_netcdf(file)
         with pytest.raises(IndexError):
             synthetic_volcanoes.create_volcanoes(version=2, option=1)
+
+
+# TODO: Add test for actually creating new, rewritten file.
+# def test_create_volcaoes(runner: CliRunner) -> None:
+#     """Test to see if files are created as expected.
+#
+#     Parameters
+#     ----------
+#     runner: CliRunner
+#         Runner for creating isolated file  system.
+#     """
+#     with runner.isolated_filesystem():
+#         # Create fake source data set
+#         os.mkdir("data")
+#         os.mkdir(os.path.join("data", "originals"))
+#         file = os.path.join(
+#             "data",
+#             "originals",
+#             "VolcanEESMv3.11_SO2_850-2016_Mscale_Zreduc_2deg_c191125.nc",
+#         )
+#         ds1 = xr.Dataset({"a": (["x", "y"], np.ones((2, 2)))})
+#         ds1.to_netcdf(file)
+#         d = os.path.join("data", "output")
+#         now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+#         out_npz = os.path.join(os.getcwd(), d, f"synthetic_volcanoes_{now}.npz")
+#         out_nc = os.path.join(os.getcwd(), d, f"synthetic_volcanoes_{now}.nc")
+#         synthetic_volcanoes.create_volcanoes(version=2, option=1)
+#         assert os.path.isfile(out_nc)
+#         assert os.path.isfile(out_npz)
+#         os.remove(out_nc)
+#         os.remove(out_npz)
