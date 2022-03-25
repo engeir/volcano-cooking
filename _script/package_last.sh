da8650ac11e200bceafcf3bb803709ae414f9ab9
engeir <eirroleng@gmail.com>
2022-03-25 15:18:27 +0100

    Add script that collects last created files
    To keep files gathered, this script can be used to collect all files
    that was created together in a reproducible way. Files are put in a new
    directory in the root of the project.

---
+++ _script/package_last.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+export DATA_SYNTH="data/output"
+export DATA_OUT="data/cesm"
+export DATA_LOG="data/cesm/logs"
+SYNTH_FILE=$(find "$DATA_SYNTH" -name "*.nc" -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+SYNTH_BASE="${SYNTH_FILE%.*}"
+SYNTH_NC="${SYNTH_BASE}.nc"
+SYNTH_NPZ="${SYNTH_BASE}.npz"
+CESM_FILE=$(find "$DATA_OUT" -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+LOG_FILE=$(find "$DATA_LOG" -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+
+if [ -z "$SYNTH_NC" ]; then
+    echo "Cannot find synthetic nc file."
+    exit 1
+fi
+if [ -z "$SYNTH_NPZ" ]; then
+    echo "Cannot find synthetic npz file."
+    exit 1
+fi
+if [ -z "$CESM_FILE" ]; then
+    echo "Cannot find CESM file."
+    exit 1
+fi
+if [ -z "$LOG_FILE" ]; then
+    echo "Cannot find log file."
+    exit 1
+fi
+
+mkdir -p source-files
+mv "$SYNTH_NC" source-files
+mv "$SYNTH_NPZ" source-files
+mv "$CESM_FILE" source-files
+mv "$LOG_FILE" source-files
