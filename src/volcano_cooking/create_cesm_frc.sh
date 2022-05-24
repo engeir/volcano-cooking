022cc1a2db5d6c542e2ee1835a0168fb4939a3c7
engeir <eirroleng@gmail.com>
2022-05-24 10:06:12 +0200

    Delete old ncl file and add .env example use of its path

--- src/volcano_cooking/create_cesm_frc.sh
+++ src/volcano_cooking/create_cesm_frc.sh
@@ -15,6 +15,7 @@ export DATA_SYNTH="data/output"
 export DATA_OUT="data/cesm"
 mkdir -p "$DATA_OUT"
 THIS_DIR="$1"
+NCL_DIR="$1"
 export NCL_SCRIPT="createVolcEruptV3.ncl"
 COORDS1DEG_FILE="fv_0.9x1.25_L30.nc"
 COORDS2DEG_FILE="fv_1.9x2.5_L30.nc"
@@ -43,7 +44,7 @@ if [ -z "$SYNTH_FILE" ]; then
     echo "Cannot find synthetic volcano forcing file. Generate with 'volcano-cooking'."
     exit 1
 fi
-if ! [ -e "$THIS_DIR/$NCL_SCRIPT" ]; then
+if ! [ -e "$NCL_DIR/$NCL_SCRIPT" ]; then
     echo "Cannot find file '$NCL_SCRIPT'."
     exit 1
 fi
@@ -83,7 +84,7 @@ RES=$RES
 Running NCL script...
 " >"$DATA_OUT"/logs/"$current_day".log
 
-ncl "$THIS_DIR/$NCL_SCRIPT" 2>&1 | tee -a "$DATA_OUT"/logs/"$current_day".log
+ncl "$NCL_DIR/$NCL_SCRIPT" 2>&1 | tee -a "$DATA_OUT"/logs/"$current_day".log
 echo "$GREEN${BOLD}Log file created at ""$DATA_OUT/logs/""$current_day.log$NORM"
 # The file need to be in NetCDF3 format. Could specify this in the ncl script, but the
 # nccopy command seems to support more formats, so perhaps it is better to use that(?).
