ced2c2b96364da1ba9bcdaa79454f39d1846bfc2
engeir <engeir@pm.me>
2022-06-28 11:20:18 -0600

    Don't use file from repo, generate instead
    Removes the use of the `./examples/json.json` file and instead create a
    similar one in an isolated file system. Better for testing.

--- tests/test_create_data.py
+++ tests/test_create_data.py
@@ -1,14 +1,35 @@
 """Test cases for the create_data module."""
 import inspect
+import json
+
+import pytest
+from click.testing import CliRunner
 
 import volcano_cooking.modules.create.create_data as cr
 
 
-def test_generate_classes() -> None:
-    module_ = "volcano_cooking.modules.create.create_data"
-    for n, c in inspect.getmembers(cr, inspect.isclass):
-        if c.__module__ == module_ and n not in ["Data", "Generate"]:
-            c_ = c(20, 20, "./examples/json.json")
-            assert issubclass(c, cr.Generate)
-            c_.generate()
-            cr.Data(*c_.get_arrays())
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces.
+
+    Returns
+    -------
+    CliRunner
+        Runner for creating isolated file  system.
+    """
+    return CliRunner()
+
+
+def test_generate_classes(runner: CliRunner) -> None:
+    """Test to see if classes are being generated."""
+    data = {"dates": ["1850-01-17", "1860-01-01"], "emissions": ["400", "400"]}
+    with runner.isolated_filesystem():
+        with open("new_file.json", "w") as f:
+            json.dump(data, f, indent=2)
+        module_ = "volcano_cooking.modules.create.create_data"
+        for n, c in inspect.getmembers(cr, inspect.isclass):
+            if c.__module__ == module_ and n not in ["Data", "Generate"]:
+                c_ = c(20, 20, "new_file.json")
+                assert issubclass(c, cr.Generate)
+                c_.generate()
+                cr.Data(*c_.get_arrays())
